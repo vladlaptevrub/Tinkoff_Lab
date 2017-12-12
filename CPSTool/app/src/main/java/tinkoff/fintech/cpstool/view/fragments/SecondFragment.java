78f253db8052a8afcaa007d23de0490237684fdb
Vladislav Laptev <vladlaptevrub@gmail.com>
2017-12-12 19:36:00 +0300

    add search field in SecondFragment

--- CPSTool/app/src/main/java/tinkoff/fintech/cpstool/view/fragments/SecondFragment.java
+++ CPSTool/app/src/main/java/tinkoff/fintech/cpstool/view/fragments/SecondFragment.java
@@ -11,8 +11,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import tinkoff.fintech.cpstool.R;
+import tinkoff.fintech.cpstool.model.history.Party;
 import tinkoff.fintech.cpstool.presenter.requests.Requests;
 import tinkoff.fintech.cpstool.view.MainActivity;
 import tinkoff.fintech.cpstool.view.adapters.RecyclerAdapter;
@@ -23,7 +31,9 @@ public class SecondFragment extends Fragment implements RecyclerAdapter.OnItemCl
     private Requests presenter;
     private RecyclerAdapter recyclerAdapter;
     private RecyclerView recyclerView;
+    private AutoCompleteTextView searchInCacheEditText;
     private MainActivity mainActivity;
+    private ArrayAdapter searchAdapter;
 
     public interface SecondFragmentListener{
         void secondCallBack(String value);
@@ -51,13 +61,23 @@ public class SecondFragment extends Fragment implements RecyclerAdapter.OnItemCl
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        String[] languages={"Android ","java","IOS","SQL","JDBC","Web services", "Anton", "Anna"};
+
         presenter = new Requests();
 
         recyclerView = (RecyclerView)getActivity().findViewById(R.id.recyclerView);
         RecyclerView.LayoutManager recyclerLayoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(recyclerLayoutManager);
         mainActivity = (MainActivity)getActivity();
+        searchInCacheEditText = (AutoCompleteTextView) getActivity().findViewById(R.id.searchInCacheEditText);
         updateRecyclerView();
+
+        searchInCacheEditText.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                listener.secondCallBack(searchAdapter.getItem(i).toString());
+            }
+        });
     }
 
     @Override
@@ -98,5 +118,18 @@ public class SecondFragment extends Fragment implements RecyclerAdapter.OnItemCl
         recyclerAdapter = new RecyclerAdapter(presenter.getData());
         recyclerView.setAdapter(recyclerAdapter);
         recyclerAdapter.setOnClick(this);
+
+        searchAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, getListOfTitles());
+        searchInCacheEditText.setAdapter(searchAdapter);
+        searchInCacheEditText.setThreshold(1);
+    }
+
+    private List<String> getListOfTitles(){
+        List<Party> parties = presenter.getData();
+        List<String> titles = new ArrayList<>();
+        for (int i = 0; i < parties.size(); i++){
+            titles.add(parties.get(i).getTitle());
+        }
+        return titles;
     }
 }
